d3bb085d364fcdb0c79460f8e5ae568c3fc6c24c
Douglas <112734316+Dhrds@users.noreply.github.com>
2023-04-22 22:21:50 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+def home():
+    return render_template("index.html")
+@app.route('/resumo')
+def resumo():
+    return render_template("resume.html")
+@app.route('/projetos')
+def projetos():
+    return render_template("projects.html")
+@app.route('/contatos')
+def contatos():
+    return render_template("contact.html")
+
+
+
+app.run(debug=True)
